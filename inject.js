91eb38081fccf25802294ea614ac4e63e0eb3897
Akash Gupta <akash.gupta1@gmail.com>
2011-09-22 21:11:57 -0700

    Fixed spacing
    That is all.

--- inject.js
+++ inject.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
 
   // Build a test map
-	var testFunction = function() { return 'accel'; }.toString();
-	localStorage["ruleTable"] = JSON.stringify({ "investors .+" : testFunction });
+  var testFunction = function() { return 'accel'; }.toString();
+  localStorage["ruleTable"] = JSON.stringify({ "investors .+" : testFunction });
 
   // Save the most recently focused upon text box
   var el;
   $('input[type="text"]').focus(function() {
     el = $(this);
-	});
+  });
 
-	window.addEventListener("keydown", function(event) {
-		var modifier = event.ctrlKey || event.metaKey;
-		if (modifier && event.shiftKey && event.keyCode == 69) {
-
-			// Read text
-			var text = el.val();
+  window.addEventListener("keydown", function(event) {
+    var modifier = event.ctrlKey || event.metaKey;
+    if (modifier && event.shiftKey && event.keyCode == 69) {
+      // Read text
+      var text = el.val();
 
       processText(el, text);
-		}
-	}, false);
+    }
+  }, false);
 });
